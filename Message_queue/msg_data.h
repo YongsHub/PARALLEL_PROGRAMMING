2c9f6c4242680e48563f9027d76a9a4e24e83919
HimJong <86949394+himJJong@users.noreply.github.com>
2021-12-15 10:01:08 +0900

    Create msg_data.h

---
+++ Message_queue/msg_data.h
@@ -0,0 +1,13 @@
+struct real_data{
+	char userID[50];
+	char password[50];
+	char sex[10];
+	char mobile[50];
+	char email[50];
+	int sequence;
+};
+
+struct message{
+	long msg_type;// 0 is insert into MYSQL DATABASE;
+	struct real_data data;
+};
